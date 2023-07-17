bb65fdddbd09fd32e1f0911078c1482f5e609153
dooli1917039 <dooli9819@gmail.com>
2023-07-18 00:41:37 +0900

    remove: RouteTest.js 삭제

--- src/App.js
+++ src/App.js
@@ -4,7 +4,6 @@ import Home from "./pages/Home";
 import Diary from "./pages/Diary";
 import Edit from "./pages/Edit";
 import New from "./pages/New";
-import RouteTest from "./components/RouteTest";
 
 function App() {
 
@@ -13,9 +12,6 @@ function App() {
           <div className="App">
               <h2>App.js</h2>
 
-              {/* process.env.PUBLIC_URL 는 public 폴더에 바로 접근할 수 있는 명령어이다. */}
-              <img src={process.env.PUBLIC_URL + `assets/emotion1.png`}/>
-
               <Routes>
                   <Route path='/' element={<Home/>} />
                   <Route path='/new' element={<New/>} />
@@ -23,7 +19,7 @@ function App() {
                   {/* :을 사용하여 : 뒤의 id라는 이름으로 값을 전달하겠다는 의미*/}
                   <Route path='/diary/:id' element={<Diary/>} />
               </Routes>
-              <RouteTest/>
+
           </div>
       </BrowserRouter>
   );
